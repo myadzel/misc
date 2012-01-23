a8158e01059fdc4a77d5c0a60b17b90a99240b32
Alexander Myadzel <myadzel@gmail.com>
2012-01-23 16:15:32 +0400

    add verbose mode for nconvert

--- Image2JPEG/cgi/convert-to-jpeg-and-resize.sh
+++ Image2JPEG/cgi/convert-to-jpeg-and-resize.sh
@@ -6,4 +6,4 @@ _dirname=$(dirname $0)
 #default image box size
 _size=${2:-500}
 
-$_dirname/NConvert/nconvert -merge_alpha -transpcolor 255 255 255 -overwrite -D -out jpeg -truecolors -rmeta -q 85 -rflag decr -ratio -resize $_size $_size\ -o $1 2>&1
+$_dirname/NConvert/nconvert -v -merge_alpha -transpcolor 255 255 255 -overwrite -D -out jpeg -truecolors -rmeta -q 85 -rflag decr -ratio -resize $_size $_size\ -o $1 2>&1
