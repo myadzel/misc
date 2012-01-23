9bb2174b099a980b203d795952f53b9094e340b6
Alexander Myadzel <myadzel@gmail.com>
2012-01-23 15:16:40 +0400

    create Image2JPEG

---
+++ Image2JPEG/cgi/convert-to-jpeg-and-resize.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#path to nconvert
+_dirname=$(dirname $0)
+
+#default image box size
+_size=${2:-500}
+
+$_dirname/NConvert/nconvert -merge_alpha -transpcolor 255 255 255 -overwrite -D -out jpeg -truecolors -rmeta -q 85 -rflag decr -ratio -resize $_size $_size\ -o $1 2>&1
